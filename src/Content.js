9d842b882f959f4d998520d3b0581359ec430481
ledo9124 <leminhdo904@gmail.com>
2023-05-03 18:28:38 +0700

    useEffect with dependencies

--- src/Content.js
+++ src/Content.js
@@ -5,25 +5,43 @@ import { useEffect , useState } from 'react'
 // - Gọi callback sau khi thêm elemet vào DOM
 //2. useEffect(callback , [])
 //- Chỉ gọi lại callback 1 lần sau khi component được mounted
-//useEffect(callback , [deps])
+//3. useEffect(callback , [deps])
+// - Callback sẽ được gọi lại mỗi khi dependencies thay đổi
 
 //1. Callback luôn được gọi sau khi component mounted
+
+const tabs = ['posts' , 'comments' , 'albums']
 
 function Content() {
 
     const [title , setTitle] = useState('')
     const [posts , setPosts] = useState([])
+    const [type , setType] = useState('posts')
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch('https://jsonplaceholder.typicode.com/' + type)
             .then(res => res.json())
             .then(posts => {
                 setPosts(posts);
             })
-    } , [])
+    } , [type])
 
     return (
         <div>
+
+            {tabs.map(tab => (
+                <button 
+                    key={tab}
+                    style={type === tab ? {
+                        color: '#fff',
+                        backgroundColor: '#333'
+                    } : {}}
+                    onClick={() => setType(tab)}
+                >
+                    {tab}
+                </button>
+            ))}
+
             <input 
                 value={title}
                 onChange={e => setTitle(e.target.value)}
@@ -31,7 +49,7 @@ function Content() {
 
             <ul>
                 {posts.map(post => (
-                    <li key={posts.id}>{post.title}</li>
+                    <li key={post.id || post.name}>{post.title || post.name}</li>
                 ))}
             </ul>
         </div>
