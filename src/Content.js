1a589f30d483e70bb89eceeae534b6434a1ee90f
ledo9124 <leminhdo904@gmail.com>
2023-05-03 19:23:52 +0700

    useEffect with DOM events

--- src/Content.js
+++ src/Content.js
@@ -10,50 +10,105 @@ import { useEffect , useState } from 'react'
 
 //1. Callback luôn được gọi sau khi component mounted
 
-const tabs = ['posts' , 'comments' , 'albums']
+
+// VD event scroll
+// const tabs = ['posts' , 'comments' , 'albums']
 
 function Content() {
 
-    const [title , setTitle] = useState('')
-    const [posts , setPosts] = useState([])
-    const [type , setType] = useState('posts')
+//     const [title , setTitle] = useState('')
+//     const [posts , setPosts] = useState([])
+//     const [type , setType] = useState('posts')
+//     const [showGoToTop , setShowGoToTop] = useState(false)
+
+//     useEffect(() => {
+//         fetch('https://jsonplaceholder.typicode.com/' + type)
+//             .then(res => res.json())
+//             .then(posts => {
+//                 setPosts(posts);
+//             })
+//     } , [type])
+
+//     useEffect(() => {
+
+//         const handleScroll = () => {
+//             setShowGoToTop(window.scrollY >= 200)
+//         }
+
+//         window.addEventListener('scroll' , handleScroll)
+
+//         //cleanup function
+//         return () => {
+//             window.removeEventListener('scroll' , handleScroll)
+//             console.log(1);
+//         }
+//     } , [])
+
+//     return (
+//         <div>
+
+//             {tabs.map(tab => (
+//                 <button 
+//                     key={tab}
+//                     style={type === tab ? {
+//                         color: '#fff',
+//                         backgroundColor: '#333'
+//                     } : {}}
+//                     onClick={() => setType(tab)}
+//                 >
+//                     {tab}
+//                 </button>
+//             ))}
+
+//             <input 
+//                 value={title}
+//                 onChange={e => setTitle(e.target.value)}
+//             />
+
+//             <ul>
+//                 {posts.map(post => (
+//                     <li key={post.id || post.name}>{post.title || post.name}</li>
+//                 ))}
+//             </ul>
+
+//             {showGoToTop && (
+//                 <button
+//                     style={{
+//                         position: 'fixed',
+//                         right: 20,
+//                         bottom: 20,
+//                     }}
+//                 >
+//                     Go To Top
+//                 </button>
+//             )}
+//         </div>
+//     )
+
+
+// VD resize
+
+    const [width , setWidth] = useState(window.innerWidth)
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/' + type)
-            .then(res => res.json())
-            .then(posts => {
-                setPosts(posts);
-            })
-    } , [type])
+
+        const handleResize = () => {
+            setWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize' , handleResize)
+
+        return () => {
+            window.removeEventListener('resize' , handleResize)
+        }
+    })
 
     return (
         <div>
-
-            {tabs.map(tab => (
-                <button 
-                    key={tab}
-                    style={type === tab ? {
-                        color: '#fff',
-                        backgroundColor: '#333'
-                    } : {}}
-                    onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-
-            <input 
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
-
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id || post.name}>{post.title || post.name}</li>
-                ))}
-            </ul>
+            <h1>{width}</h1>
         </div>
     )
+
 }
 
 export default Content;
