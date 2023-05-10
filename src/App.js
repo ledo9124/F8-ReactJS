1b5cb2057dc2227a67709e31c112a5f7aedc9442
ledo9124 <leminhdo904@gmail.com>
2023-05-10 17:32:09 +0700

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState , useCallback } from 'react'
-import Content from './Content'
+import { useState , useMemo , useRef } from 'react'
+// import Content from './Content'
  
 // const gifts = [
 //   'CPU i9' ,
@@ -183,18 +183,72 @@ function App() {
 
   //useEffect
 
-  const [count , setCount] = useState(0)
+  // const [count , setCount] = useState(0)
 
-  const handleIncrease = useCallback(() => {
-    setCount(pver => pver + 1)
-  }, [])  
+  // const handleIncrease = useCallback(() => {
+  //   setCount(pver => pver + 1)
+  // }, [])  
+
+  // return (
+  //   <div style={{ padding: 20}}>
+  //     <Content 
+  //       onIncrease={handleIncrease} 
+  //     />
+  //     <h1>{count}</h1>
+  //   </div>
+  // )
+
+  const [name , setName] = useState('')
+  const [price , setPrice] = useState('')
+  const [products , setProducts] = useState([])
+
+  const nameRef = useRef()
+
+  const handleSubmit = () => {
+    setProducts([
+      ...products , {
+        name,
+        price: +price
+      }
+    ])
+    setName('')
+    setPrice('')
+
+    nameRef.current.focus()
+  }
+
+  const total = useMemo(() => {
+    const result = products.reduce((result , prod) => 
+      result + prod.price,
+      0
+    )
+
+    return result
+  } , [products])
 
   return (
-    <div style={{ padding: 20}}>
-      <Content 
-        onIncrease={handleIncrease} 
+    <div style={{ padding:20 }}>
+      <input 
+        ref={nameRef}
+        value={name}
+        placeholder='Enter name...'
+        onChange={e => setName(e.target.value)}  
       />
-      <h1>{count}</h1>
+      <br />
+      <input 
+        value={price}
+        placeholder='Enter price...'
+        onChange={e => setPrice(e.target.value)}  
+      />
+      <br />
+      <button onClick={handleSubmit} >Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {products.map((product , index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 
